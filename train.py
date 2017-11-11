b223519486dc120957aa412ba2ef1d921c5d0670
Adam Cribbs <adam.cribbs@imm.ox.ac.uk>
2017-11-11 11:18:07 +0000

    Have added code to the script so on monday when we meet we can check the code and parameters, debug then train

--- train.py
+++ train.py
@@ -6,145 +6,321 @@ and
 https://github.com/tensorflow/tensorflow/blob/r1.3/tensorflow
 /examples/tutorials/mnist/mnist_with_summaries.py
 '''
+
+'''
+Refer to https://medium.com/onfido-tech/higher-level-apis-in-tensorflow-67bfb602e6c0
+Basically, we will use high level tensorflow's API: Estimator + Experiment + Dataset
+'''
+from __future__ import division, print_function
+
 import tensorflow as tf
-import os
-import sys
-import argparse
+from tensorflow.contrib.learn import learn_runner
+from tensorflow.contrib import rnn
+from tensorflow.contrib.learn import ModeKeys
+from tensorflow.contrib import slim
+import numpy as np
 import h5py
 import scipy.io
 
-def train():
+
+# tf needs >= 1.3.0
+assert tf.__version__>='1.3.0', 'tensorflow version needs to be no lower than 1.3.0'
+
+tf.logging.set_verbosity(tf.logging.DEBUG)
+
+
+# Set up the FLAGS, which is basically a wrapper for argparse. The aim of this
+# is to write demo code. It may be better for the future to implement arg parsing
+# with argparse in the future if this code becomes stable
+
+FLAGS = tf.app.flags.FLAGS
+
+# Define the model and data directories i.e. model_dir where model will be saved
+tf.app.flags.DEFINE_string(flag_name='model_dir', default_value='', docstring='')
+tf.app.flags.DEFINE_string(flag_name='data_dir', default_value='', docstring='')
+
+
+# In the future it may be worth parameterising this with an ini file
+
+def run_experiment(argv=None):
+    '''Run the experiment'''
+    
+    # Define the hyperparameters
+    params = tf.contrib.training.HParams(
+        learning_rate=0.001,
+        n_classes=919,
+        train_steps=5000,
+        min_eval_frequency=100)
+    
+    # set the run config and directory to save the model and stats
+    run_config = tf.contrib.learn.RunConfig()
+    run_config = run_config.replace(model_dir=FLAGS.model_dir)
+    
+    learn_runner.run(
+        experiment_fn=experiment_fn,
+        run_config=run_config,
+        schedule="train_and_evaluate",
+        hparams=params)
+        
+        
+def experiment_fn(run_config, params):
+    '''Create and experiment to train and evaluate the model'''
+    
+    run_config = run_config.replace(
+                    save_checkpoints_steps=params.min_eval_frequency)
+    
+    # Define the classifier
+    
+    estimator = get_estimator(run_config, params)
+    
+    # Setup data loaders
+    danq_train = train_input_fn, train_input_hook = get_train_inputs(
+            batch_size=100, data=trainmat, test=False)
+    
+    danq_test = eval_input_fn, eval_input_hook = get_train_inputs(
+            batch_size=100, data=trainmat, test=True)
+    
+    # Define the experiment
+    
+    experiment = tf.contrib.learn.Experiment(
+        estimator=estimator,
+        train_input_fn=train_input_fn,
+        eval_input_fn=eval_input_fn,
+        train_steps=params.train_steps,
+        min_eval_frequency=params.min_eval_frequency,
+        train_monitors=[train_input_hook],
+        eval_hooks=[eval_input_hook],
+        eval_steps=None)
+    
+    return experiment
+    
+def get_estimator(run_config, params):
+    '''Return the model as a tensorflow object'''
+    
+    return tf.estimator.Estimator(
+            model_fn=model_fn,
+            params=params,
+            config=run_config)
+
+
+def model_fn(features, labels, mode, params):
+    '''Model function is used in the estimator and is required for running model'''
+    
+    is_training = mode == ModeKeys.TRAIN
+    
+    # Define the models architecture
+    logits = architecture(features, is_training=is_training)
+    predictions = tf.argmax(logits, axis=1)
+    
+    # Loss functions and not needed during inference
+    
+    loss = None
+    train_op = None
+    eval_metric_ops = {}
+    
+    if mode != ModeKeys.INFER:
+        loss = tf.losses.sparse_softmax_cross_entropy(labels=tf.cast(labels, tf.int32), logits=logits)
+        
+        train_op = get_train_op_fn(loss, params)
+        eval_metric_ops = get_eval_metric_ops(labels, predictions)
+    return tf.estimator.EstimatorSpec(mode=mode,
+                                     predictions=predictions,
+                                     loss=loss,
+                                     train_op=train_op,
+                                     eval_metric_ops=eval_metric_ops)
+                                     
+def get_train_op_fn(loss, params):
+    """Get the training Op.
+
+    Args:
+         loss (Tensor): Scalar Tensor that represents the loss function.
+         params (HParams): Hyperparameters (needs to have `learning_rate`)
+
+    Returns:
+        Training Op
+    """
+    return tf.contrib.layers.optimize_loss(
+        loss=loss,
+        global_step=tf.contrib.framework.get_global_step(),
+        optimizer=tf.train.AdamOptimizer,
+        learning_rate=params.learning_rate
+    )
+
+
+def get_eval_metric_ops(labels, predictions):
+    """Return a dict of the evaluation Ops.
+
+    Args:
+        labels (Tensor): Labels tensor for training and evaluation.
+        predictions (Tensor): Predictions Tensor.
+    Returns:
+        Dict of metric results keyed by name.
+    """
+    return {
+        'Accuracy': tf.metrics.accuracy(
+            labels=labels,
+            predictions=predictions,
+            name='accuracy')
+    }
+
+def BDNN(x):
+    '''Bidirectional neural network'''
+    
+    forward_lstm = rnn.LSTMCell(320, reuse=tf.get_variable_scope().reuse)
+    backward_lstm = rnn.LSTMCell(320, reuse=tf.get_variable_scope().reuse)
+    
+    brnn, _ = tf.nn.bidirectional_dynamic_rnn(forward_lstm, backward_lstm, x, dtype=tf.float32)
+
+    return brnn
+
+def architecture(inputs, is_training, scope='DanQNN'):
+    
     '''
-    build the model
-    conv1d: input_dim=4,
-            input_length=1000,
-            nb_filter=1024,
-            filter_length=30,
-            border_mode="valid",
-            activation="relu",
-            subsample_length=1 
-    note that conv1d will be initialised with JASPER  
+    This function creates a CNN followed by a bidirectional LSTM RNN as per DanQ publication
+    We aim to have this implimented in tensorflow as it will be easier to
+    modify the implimentation for other uses if we incorporate with tensorboard.
     '''
-    # input data
-    trainmat = h5py.File('data/deepsea_train/train.mat')
-    validmat = scipy.io.loadmat('data/deepsea_train/valid.mat')
-    testmat = scipy.io.loadmat('data/deepsea_train/test.mat')
     
-    X_train = np.transpose(np.array(trainmat['trainxdata']), axes=(2,0,1))
-    y_train = np.transpose(trainmat['traindata']).T
+    with tf.variable_scope(scope):
 
-    # define variables 
-    learning_rate = 0.001
-    num_steps = 2000
-    batch_size = 1000
+        conv1d = tf.layers.conv1d(x, filters=nb_filter , strides=subsample, 
+                                  padding=border, kernel_size=input_length, data_format='channels_first', reuse=reuse)
 
-    input_dim = 4
-    input_length = 1000
-    num_classes = 919
-    dropout_cnn = 0.2
+        max1 = tf.layers.max_pooling1d(conv1d, pool_size=max_pool_size, strides=max_strides)
+        
+        max1 = tf.layers.dropout(max1, rate=0.2,training=mode == tf.estimator.ModeKeys.TRAIN)
+        
 
-    sess = tf.InteractiveSession()
-    # Input placeholders
-    with tf.name_scope('input'):
-        x = tf.placeholder(tf.float32, [None, input_dim], name='x-input')
-        y_ = tf.placeholder(tf.float32, [None, num_classes], name='y-input')
+        brnn = BDNN(max1)
+        
+        brnn = tf.layers.dropout(brnn, rate=0.5,training=mode == tf.estimator.ModeKeys.TRAIN)
 
-    # We can't initialize these variables to 0 - the network will get stuck.
-    def weight_variable(shape):
-        """Create a weight variable with appropriate initialization."""
-        initial = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(initial)
+        brnn = tf.contrib.layers.flatten(brnn)
+        
+        fc1 = tf.layers.dense(brnn, units=925, activation=tf.nn.relu)
+        
+        fc2 = tf.layers.dense(fc1, units=919, activation=tf.nn.softmax)
+        return fc2
 
-    def bias_variable(shape):
-        """Create a bias variable with appropriate initialization."""
-        initial = tf.constant(0.1, shape=shape)
-        return tf.Variable(initial)
 
-    def conv1d(x, W, b, strides=1):
+# Define data loaders #####################################
+class IteratorInitializerHook(tf.train.SessionRunHook):
+    """Hook to initialise data iterator after Session is created."""
+
+    def __init__(self):
+        super(IteratorInitializerHook, self).__init__()
+        self.iterator_initializer_func = None
+
+    def after_create_session(self, session, coord):
+        """Initialise the iterator after the session has been created."""
+        self.iterator_initializer_func(session)
+
+# Define the training inputs
+def get_train_inputs(batch_size, data, test=False):
+    """Return the input function to get the training data.
+    Args:
+        batch_size (int): Batch size of training iterator that is returned
+                          by the input function.
+        data (Object): Object holding the loaded data.
+        test (boolean): if test, then load valid mat for testing purposes
+    Returns:
+        (Input function, IteratorInitializerHook):
+            - Function that returns (features, labels) when called.
+            - Hook to initialise input iterator.
+    """
+    iterator_initializer_hook = IteratorInitializerHook()
+
+    def train_inputs():
+        """Returns training set as Operations.
+        Returns:
+            (features, labels) Operations that iterate over the dataset
+            on every evaluation
         """
-        Conv1d wrapper with bias and relu activation
-        
-        x : tensor
-        W : filter
-        b : bias
-        stride : number of filter is moved
+        with tf.name_scope('Training_data'):
+            # Get  data
+            if test:
+                DNA = data['validxdata']
+                labels = data['validdata']
+            else:
+                DNA = data['trainxdata']
+                labels = data['traindata']
+            # Define placeholders
+            DNA_placeholder = tf.placeholder(
+                DNA.dtype, DNA.shape)
+            labels_placeholder = tf.placeholder(
+                labels.dtype, labels.shape)
+            # Build dataset iterator
+            dataset = tf.contrib.data.Dataset.from_tensor_slices(
+                (DNA_placeholder, labels_placeholder))
+            dataset = dataset.repeat(None)  # Infinite iterations
+            dataset = dataset.shuffle(buffer_size=10000)
+            dataset = dataset.batch(batch_size)
+            iterator = dataset.make_initializable_iterator()
+            next_example, next_label = iterator.get_next()
+            # Set runhook to initialize iterator
+            iterator_initializer_hook.iterator_initializer_func = \
+                lambda sess: sess.run(
+                    iterator.initializer,
+                    feed_dict={images_placeholder: DNA,
+                               labels_placeholder: labels})
+            # Return batched (features, labels)
+            return next_example, next_label
+
+    # Return function and hook
+    return train_inputs, iterator_initializer_hook
+
+    def get_test_inputs(batch_size, data, test=False):
+        """Return the input function to get the test data.
+        Args:
+                    batch_size (int): Batch size of training iterator that is returned
+                          by the input function.
+            data (Object): Object holding the loaded data.
+            test (boolean): if test, then load valid mat for testing purposes
+        Returns:
+            (Input function, IteratorInitializerHook):
+                - Function that returns (features, labels) when called.
+                - Hook to initialise input iterator.
         """
-        x = tf.nn.conv1d(x, W, strides, "VALID")
-        x = tf.nn.bias_add(x, b)
-        
-        return tf.nn.relu(x)
-    
-    def maxpool1D(x, pool_size=13, strides=13, k=1):
+        iterator_initializer_hook = IteratorInitializerHook()
+
+    def test_inputs():
+        """Returns training set as Operations.
+        Returns:
+            (features, labels) Operations that iterate over the dataset
+            on every evaluation
         """
-        return a 1d max pooling layer
-        
-        value : tensor of the format specified by data format
-        ksize : The size of window for input of tensor
-        strides : The stride of sliding window
-        
-        """
-        return tf.nn.max_pooling1d(inputs=x, pool_size=pool_size, strides=strides)
+        with tf.name_scope('Test_data'):
+            # Get data
+            if test:
+                DNA = data['validxdata']
+                labels = data['validdata']
+            else:
+                DNA = data['testxdata']
+                labels = data['testdata']
+            # Define placeholders
+            DNA_placeholder = tf.placeholder(
+                DNA.dtype, DNA.shape)
+            labels_placeholder = tf.placeholder(
+                labels.dtype, labels.shape)
+            # Build dataset iterator
+            dataset = tf.contrib.data.Dataset.from_tensor_slices(
+                (DNA_placeholder, labels_placeholder))
+            dataset = dataset.batch(batch_size)
+            iterator = dataset.make_initializable_iterator()
+            next_example, next_label = iterator.get_next()
+            # Set runhook to initialize iterator
+            iterator_initializer_hook.iterator_initializer_func = \
+                lambda sess: sess.run(
+                    iterator.initializer,
+                    feed_dict={images_placeholder: DNA,
+                               labels_placeholder: labels})
+            return next_example, next_label
 
-    def conv_net(x, weights, biases, dropout):
-        
-        conv1d = conv1d(x, weights, biases, strides = 1)
-        conv1d = maxpool1D(conv1, k=1)
-        
-        ## Fully connected layer
-        
-        # Reshape to fit fully connected layer
-        fc1 = tf.reshape()
-        fc1 = tf.add(tf.matmul(fc1, weights['w1']), biases['b1'])
-        fc1 = tf.nn.relu(fc1)
-        
-        # Apply dropout
-        
-        fc1 = tf.nn.dropout(fc1, dropout)
-        
-        # Output class prediction
-        
-        out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
-        
-        return out
+    # Return function and hook
+    return test_inputs, iterator_initializer_hook
 
-
-# initialise the weights
-weights = {'w1' : tf.Variable(tf.random_normal([])),
-           'out' : tf.Variable(tf.random_normal([]))}
-
-# initalise biase with size of output 
-biases = {'b1' : tf.Variable(tf.random_normal([])),
-         'out' : tf.Variable(tf.random_normal([]))}
-
-# Construct model
-
-logits = conv_net(X_train, weights, biases, dropout)
-prediction = tf.nn.sigmoid(logits)
-
-# Define loss and optimizer
-loss_op = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=y_train))
-optimiser = tf.train.AdamOptimizer(learning_rate=learning_rate)
-train_op = optimiser.minimize(loss_op)
-
-# Evaluate model
-
-correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(y_train, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-# initialise the variables
-
-init = tf.global_variables_initializer()
-
- 
-def main(**FLAGS):
-    # parameters:
-    #  training data
-    #  valid data
-    #  test data
-    #  max epochs = 32
-    #  using JASPER = True
-    # load training data
-
-if __name__ == '__main__':
-    
-    main(**FLAGS)
+# Run script ##############################################
+if __name__ == "__main__":
+    tf.app.run(
+        main=run_experiment
+    )
